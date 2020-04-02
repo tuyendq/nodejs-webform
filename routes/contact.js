bfd96a709a8f4741afa79db731ddb75098f6f53f
Tuyen Dang <tuyendq@gmail.com>
2020-04-02 23:06:30 +0700

    form field invalid

--- routes/contact.js
+++ routes/contact.js
@@ -5,7 +5,24 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('contact');
+    res.render('contact', { 
+        data: {}, 
+        errors: {}
+    });
+});
+
+router.post('/', (req, res) => {
+    res.render('contact', {
+        data: req.body,
+        errors: {
+            message: {
+                msg: 'A message is required.'
+            },
+            email: {
+                msg: 'That email address does not look right.'
+            }
+        }
+    });
 });
 
 
